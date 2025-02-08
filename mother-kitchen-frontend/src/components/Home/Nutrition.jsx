4ba8497c2483f161b87400d03b553ceb903506a9
Aakash Pal <akashpal1482@gmail.com>
2025-02-08 10:12:09 +0530

    changes reversed

--- mother-kitchen-frontend/src/components/Home/Nutrition.jsx
+++ mother-kitchen-frontend/src/components/Home/Nutrition.jsx
@@ -19,7 +19,9 @@ const NutritionSm = ({ navigate }) => {
               <span className="white-shadow font-gluten text-2xl font-black text-red-base"> in your dish</span>
             </h3>
             <p className="text-justify font-lexend text-xs md:text-sm">
-            "At Mother's Kitchen, love is our secret ingredient. With simple, high-quality products you are relieved form a lot of worries, leaving you more time for family. Our recipes, passed down by generations of moms, bring comfort and warmth to every bite. Join us in the kitchen and taste the love in every meal!
+              At Mother&rsquo;s Kitchen, love is always the first ingredient. Our top-quality ingredients and
+              easy-to-use products make cooking a breeze, so you can spend more time with the people who matter most.
+              From seasonings and spices to baking mixes and ready-to-eat meals, we&rsquo;ve got everything you need to whip up something delicious.
             </p>
             <div className="flex justify-end">
               <Button className="my-5" onClick={() => navigate('/about')}>
@@ -53,7 +55,11 @@ const NutritionMd = ({ navigate }) => {
               <span className="white-shadow font-gluten text-3xl font-black text-red-base"> in your dish</span>
             </h3>
             <p className="pr-4 text-justify font-lexend text-xs font-medium leading-4 tracking-[-0.01rem]">
-            "At Mother's Kitchen, love is our secret ingredient. With simple, high-quality products you are relieved form a lot of worries, leaving you more time for family. Our recipes, passed down by generations of moms, bring comfort and warmth to every bite. Join us in the kitchen and taste the love in every meal!
+              At Mother&apos;s Kitchen, love is always the first ingredient. We believe that food is not just fuel for
+              the body, but also nourishment for the soul. We are passionate about creating products that are not only
+              healthy and delicious, but also bring people together and foster a sense of community and connection. Our
+              recipes are passed down through generations of moms, ensuring that every food product is packed with
+              warmth and love.
             </p>
             <Button className="mt-10" onClick={() => navigate('/about')}>
               View More
@@ -68,25 +74,30 @@ const NutritionMd = ({ navigate }) => {
 const NutritionLg = ({ navigate }) => {
   return (
     <>
-      <section className="md:mx-20">
+      <section className="md:mx-20 2xl:mx-56">
         <YellowDiv className="flex">
           <div className="relative flex-1">
             <img
               loading="lazy"
               src={Nutrition}
               alt="nutrition"
-              className="absolute -top-10 h-full w-[100%]  object-top"
+              className="absolute -top-10 h-full w-[100%] object-contain object-top"
             />
           </div>
-          <div className="z-10 p-4">
-            <h3 className="mb-4 font-lexend text-[50px] font-semibold tracking-[-0.2rem] w-[610px]">
+          <div className="z-10 flex-1 p-5">
+            <h3 className="mb-5 font-lexend text-4xl font-semibold tracking-[-0.2rem]">
               Pouring nutrition <br />{' '}
-              <span className="mt-2 font-gluten text-[56px] font-black tracking-normal text-red-base"> in your dish</span>
+              <span className="mt-2 font-gluten text-4xl font-black tracking-normal text-red-base"> in your dish</span>
             </h3>
-            <p className="text-justify font-lexend text-base font-medium tracking-[-0.1rem] text-[26px] w-[600px] leading-[35px]">
-            "At Mother's Kitchen, love is our secret ingredient. With simple, high-quality products you are relieved form a lot of worries, leaving you more time for family. Our recipes, passed down by generations of moms, bring comfort and warmth to every bite. Join us in the kitchen and taste the love in every meal!
+            <p className="pr-4 text-justify font-lexend text-base font-medium tracking-[-0.1rem]">
+              At Mother&apos;s Kitchen, love is always the first ingredient. We believe that food is not just fuel for
+              the body, but also nourishment for the soul. We are passionate about creating products that are not only
+              healthy and delicious, but also bring people together and foster a sense of community and connection. Our
+              recipes are passed down through generations of moms, ensuring that every food product is packed with
+              warmth and love.so whether you&apos;re whipping up a quick snack or preparing a feast for your loved ones,
+              We&apos;ve has got you covered with our range of pure and hygienic products.
             </p>
-            <Button className="mt-5 p-2 text-[1rem]" onClick={() => navigate('/about')}>
+            <Button className="mt-10" onClick={() => navigate('/about')}>
               View More
             </Button>
           </div>
